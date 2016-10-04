1c95270d59fff22d25beaf40ad6b1c8197aadd9c
pablo-a <pabloabril@hotmail.fr>
2016-10-04 16:26:52 +0200

    on avance bien sur la connexion/deco bientot fini (envisager de faire des sessions ?)

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+include_once("connect_bdd.php");
+
+function display_connected()
+{
+	echo '<script>document.getElementById("connected").style.display = "none";
+								document.getElementById("not_connected").style.display = "block";</script>';
+}
+
+function display_deconnected()
+{
+	echo '<script>document.getElementById("connected").style.display = "block";
+								document.getElementById("not_connected").style.display = "none";</script>';
+}
+
+?>
+
+
+
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -23,7 +42,8 @@
 				<li><a href="gallerie.php">GALLERIE</a></li>
 				<li><a href="#about">ABOUT</a></li>
 				<li class="right"><a href="suscribe.php">INSCRIPTION</a></li>
-				<li class="right"><a href="#connect.php" onclick="show_signin()">CONNEXION</a></li>
+				<li class="right" id="not_connected"><a href="#connect.php" onclick="show_signin()">CONNEXION</a></li>
+				<li class="right hidden" id="connected"><a href="index.php?signin=out">DECONNEXION</a></li>
 			</ul>
 		</div>
 
@@ -32,9 +52,9 @@
 			<div id="connexion">
 				<p>CONNEXION</p>
 				<a href="#"><img src="img/red_cross.png" alt="quit" onclick="hide_signin()" width="30px" height="30px"/></a>
-				<form class="connexion" action="index.php" method="post">
-						<input type="text" name="pseudo" placeholder="pseudo">
-						<input type="password" name="passwd" placeholder="password">
+				<form class="connexion" action="index.php?signin=in" method="post">
+						<input type="text" name="pseudo" placeholder="pseudo" required>
+						<input type="password" name="passwd" placeholder="password" required>
 						<input type="submit" name="submit" value="OK">
 				</form>
 				<div id="other">
@@ -64,6 +84,37 @@ while ($data = $querry->fetch())
 }
 $querry->closeCursor();
 
+if (isset($_GET['signin']) && $_GET['signin'] == 'in')
+{
+
+	if (!empty($_POST['pseudo']) && !empty($_POST['passwd']))
+	{
+			$requete = "SELECT * FROM User WHERE pseudo = ?";
+			$search_user = $bdd->prepare($requete);
+			$search_user->execute(array($_POST['pseudo']));
+			if ((int)$search_user->rowCount() == 1 )
+			{
+				$result = $search_user->fetch();
+				if ($result['passwd'] == hash("whirlpool", $_POST['passwd']))
+						display_deconnected();
+				else {
+						echo "<h2>Bad Password</h2>";
+				}
+			}
+			else {
+				echo "<h2>Error, User not found.</h2>";
+			}
+
+	}
+	else {
+			echo "<h2>veuillez remplir tous les champs.</h2>";
+	}
+}
+
+if (isset($_GET['signin']) && $_GET['signin'] == 'out')
+{
+		display_connected();
+}
 ?>
 			<div class="footer">
 				<p>pabril copyright</p>
