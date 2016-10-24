2f6a3ad8c3ecb58eefb2cf4839fc041aaafd3eed
pablo-a <pabloabril@hotmail.fr>
2016-10-24 14:52:34 +0200

    le reset de mot de passe est presque fini

--- config/setup.sql
+++ config/setup.sql
@@ -4,7 +4,7 @@ USE camagru;
 
 SET NAMES utf8;
 
-CREATE TABLE IF NOT EXISTS User (
+CREATE TABLE IF NOT EXISTS user (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	pseudo VARCHAR(50) NOT NULL,
 	password VARCHAR(300) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS User (
 
 
 
-CREATE TABLE IF NOT EXISTS Image (
+CREATE TABLE IF NOT EXISTS image (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	location VARCHAR(100) NOT NULL,              -- "path/to/file"
 	owner INT UNSIGNED NOT NULL,
@@ -30,12 +30,12 @@ CREATE TABLE IF NOT EXISTS Image (
 	name VARCHAR(30) NOT NULL,                   -- champ facultatif
 	description TEXT,                            -- champ facultatif
 	PRIMARY KEY (id),
-	FOREIGN KEY (owner) REFERENCES User (id)     -- le champs owner refere a un id dans la table user.
+	FOREIGN KEY (owner) REFERENCES user (id)     -- le champs owner refere a un id dans la table user.
 ) ENGINE=InnoDB;
 
 
 
-CREATE TABLE IF NOT EXISTS comments (
+CREATE TABLE IF NOT EXISTS comment (
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	content TEXT NOT NULL,								-- corps du commentaire
 	author INT UNSIGNED NOT NULL,         -- auteur du commentaire
@@ -43,10 +43,17 @@ CREATE TABLE IF NOT EXISTS comments (
 	image_origin INT UNSIGNED NOT NULL,   -- image liée au commentaire
 	comment_origin INT UNSIGNED ,         -- nested comments (facultatif)
 	PRIMARY KEY (id),
-	CONSTRAINT fk_comment_author FOREIGN KEY (author) REFERENCES User(id),
-	CONSTRAINT fk_comment_image FOREIGN KEY (image_origin) REFERENCES Image(id)
+	CONSTRAINT fk_comment_author FOREIGN KEY (author) REFERENCES user(id),
+	CONSTRAINT fk_comment_image FOREIGN KEY (image_origin) REFERENCES image(id)
 ) ENGINE=InnoDB;
 
+CREATE TABLE IF NOT EXISTS reset (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	id_user INT UNSIGNED NOT NULL,
+	num VARCHAR(200) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_user) REFERENCES user (id)
+) ENGINE=InnoDB;
 
 INSERT INTO User (pseudo, password, mail, creation_time, active, admin)
 	VALUES ('pablo', 'pablo', 'pablo@abril.fr', 20160927111111, 1, 1);
--- reset_pwd.php
+++ reset_pwd.php
@@ -1,15 +1,86 @@
+<?php
+//IMPORTATION DE LA FONTION DE CONNEXION A LA BDD
+include_once('connect_bdd.php');
+
+//CONNEXION A LA BDD
+$bdd = connect_bdd($DB_DSN, $DB_USER, $DB_PASSWORD);
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <title>Reinitialiser son mot de passe.</title>
+        <link rel="stylesheet" href="css/header.css" type="text/css" />
+        <link rel="stylesheet" href="css/navbar.css" type="text/css" />
+        <link rel="stylesheet" href="css/signin.css" type="text/css" />
+        <link rel="stylesheet" href="css/montage.css" type="text/css" />
     </head>
     <body>
-        <h1>Reinitialiser son mot de passe.</h1>
-        <h3>Remplissez les champs suivants.</h3>
-        <form class="reset_pwd" action="#" method="post">
-            <input type="text" name="mail" placeholder="mail">
-            <input type="submit" name="submit" value="Envoyer">
-        </form>
+        <?php include_once("include/header.php"); ?>
+        <?php include_once("include/navbar.php"); ?>
+        <div class="body">
+            <?php include_once("include/signin.php"); ?>
+
+<?php if (extract($_GET)) { ?>             <!-- Nouveau password (PART 2) -->
+            <h1>nouveau mot de passe</h1>
+            <form class="new_pwd" action="#" method="post">
+                <input type="password" name="passwd1" placeholder="mdp">
+                <input type="password" name="passwd2" placeholder="confirm">
+                <input type="submit" name="submit" value="Changer">
+            </form>
+
+<?php }
+else {                                       // Envoi de mail (PART 1)
+?>
+            <h1>Reinitialiser son mot de passe.</h1>
+            <h3>Remplissez les champs suivants.</h3>
+            <form class="reset_pwd" action="#" method="post">
+                <input type="email" name="mail" placeholder="mail">
+                <input type="submit" name="submit" value="Envoyer">
+            </form>
+        </div>
     </body>
 </html>
+
+<?php
+}
+
+//EMVOYER MAIL
+
+if (extract($_POST))
+{
+    if (isset($mail))
+    {
+        $id_reset = hash("whirlpool", $mail);
+
+        $requete_find_user_id = "SELECT * FROM user WHERE mail = ?";
+        $query_user = $bdd->prepare($requete_find_user_id);
+        $query_user->execute(array($mail));
+        if ((int)$query_user->rowCount() == 1)
+        {
+            $result = $query_user->fetch();
+            $id_user = $result['id'];
+            $query_user->closeCursor();
+
+            $insertion = $bdd->prepare("INSERT INTO reset (id_user, num) VALUES (:id_user, :id_reset)");
+            $insertion->execute(array('id_user' => $id_user, 'id_reset' => $id_reset));
+            $insertion->closeCursor();
+
+            $link = $_SERVER['SERVER_NAME'] . ":8080" . $_SERVER['PHP_SELF'] . "?id_reset=" . $id_reset;
+
+            $content = "Voici un lien pour reinitialiser votre mot de passe :\n" . $link;
+
+            mail($mail, "reset password", $content);
+        }
+        else {
+            echo "<h2> Mail not found in database.</h2>";
+        }
+    }
+    else {
+        
+        echo "new";
+    }
+}
+
+?>
