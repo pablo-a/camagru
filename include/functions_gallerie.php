25751f8f09d9af7a4b5ce72ba51a9c5a88d2977b
pablo-a <pabloabril@hotmail.fr>
2016-11-09 13:59:59 +0100

    gestion des inputs utilisateurs pour les images a save et les commentaires

--- include/functions_gallerie.php
+++ include/functions_gallerie.php
@@ -24,34 +24,43 @@ function get_user_by_id ($id, $bdd) {
     }
 }
 
-if (isset($_POST) && extract($_POST) && $comment && isset($_GET['id'])) { // COMMENT
+if (isset($_POST) && extract($_POST) && $comment && isset($_GET['id'])) { // COMMENT A INSERER
 
     //verif si commentaire bien OK
+    if (strlen($comment) > 200) {
+      banner_alert("Commentaire trop long.");
+    }
 
+    $pattern = "/[^a-zA-Z1-9-]/";
+    elseif (preg_match($pattern, $comment)) {
+        banner_alert("caracteres interdits ! seul les lettres et chiffres sont acceptes");
+    }
+    else {
+        //ajout du commentaire dans BDD.
+        $insert_comment = $bdd->prepare("INSERT INTO comment (content, author, creation_time, image_origin)
+                        VALUES (:content, :author, :creation_time, :image_origin)");
+        $result_insert = $insert_comment->execute(array('content' => $comment,
+                                                        'author' => $_SESSION['user_id'],
+                                                        'creation_time' => date("YmdHis"),
+                                                        'image_origin' => $_GET['id']));
+        $insert_comment->closeCursor();
+        $update_comment_nb = $bdd->prepare("UPDATE image SET comments_nb = comments_nb + 1 WHERE id = ?");
+        $update_comment_nb->execute(array($_GET['id']));
+        $update_comment_nb->closeCursor();
 
-    //ajout commentaire a la base de donnees.
-    $insert_comment = $bdd->prepare("INSERT INTO comment (content, author, creation_time, image_origin)
-                    VALUES (:content, :author, :creation_time, :image_origin)");
-    $result_insert = $insert_comment->execute(array('content' => $comment,
-                                                    'author' => $_SESSION['user_id'],
-                                                    'creation_time' => date("YmdHis"),
-                                                    'image_origin' => $_GET['id']));
-    $insert_comment->closeCursor();
-    $update_comment_nb = $bdd->prepare("UPDATE image SET comments_nb = comments_nb + 1 WHERE id = ?");
-    $update_comment_nb->execute(array($_GET['id']));
-    $update_comment_nb->closeCursor();
+        //ENVOI D'UN MAIL AU PROPRIETAIRE DE LA PHOTO
+        $user = $bdd->query("SELECT mail from user where id = (SELECT owner from image WHERE id = " . $_GET['id'] . ")");
+        $user_email = $user->fetch();
+        $mail = $user_email['mail'];
+        $subject = "Quelqu'un a commente une de vos photos !";
+        $url = "localhost:8080/camagru/gallerie.php?id=" . $_GET['id'];
+        $content = "L'utilisateur " . $_SESSION['user_name'];
+        $content .= " a commente une de vos photos :\n" . $url;
+        mail($mail, $subject, $content);
+        $user->closeCursor();
+        banner_alert("un email a ete envoye au proprietaire de cette image.");
+    }
 
-    //ENVOI D'UN MAIL AU PROPRIETAIRE DE LA PHOTO
-    $user = $bdd->query("SELECT mail from user where id = (SELECT owner from image WHERE id = " . $_GET['id'] . ")");
-    $user_email = $user->fetch();
-    $mail = $user_email['mail'];
-    $subject = "Quelqu'un a commente une de vos photos !";
-    $url = "localhost:8080/camagru/gallerie.php?id=" . $_GET['id'];
-    $content = "L'utilisateur " . $_SESSION['user_name'];
-    $content .= " a commente une de vos photos :\n" . $url;
-    mail($mail, $subject, $content);
-    $user->closeCursor();
-    banner_alert("un email a ete envoye au proprietaire de cette image.");
 }
 
 else if (isset($_GET['delid']) && !empty($_GET['delid']) && extract($_GET)) { // SUPPRESION PHOTO
--- webcam.php
+++ webcam.php
@@ -3,74 +3,81 @@
 
 
 
-if (extract($_POST) && $hidden && isset($filtre) && !empty($name)) // Photo recue du formulaire (upload ou webcam)
+if (extract($_POST) && $hidden) // Photo recue du formulaire (upload ou webcam)
 {
-    // creation d'un repertoire au nom de l'utilisateur si il n'en a pas.
-    if (!is_dir("img/" . $_SESSION['user_name']))
-    {
-        mkdir("img/" . $_SESSION['user_name']);
+    //GESTION DES ERREURS FORMULAIRE.
+    $pattern = "/[^a-zA-Z1-9-]/";
+    if (empty($name)) {
+        banner_alert("Vous devez renseigner un nom a votre photo pour l'enregistrer.");
     }
-
-    // ON cree une path pour le fichier a enregistrer.
-    $location = "img/" . $_SESSION['user_name'] . "/photo" . date("YmdHis") . ".png";
-    // On trouve l'id de l'utilisateur.
-    $query_user_id = $bdd->prepare("SELECT id FROM user WHERE pseudo = ?");
-    $query_user_id->execute(array($_SESSION['user_name']));
-    $result_user_id = $query_user_id->fetch();
-    $id_user = $result_user_id['id'];
-
-    //on transfere les donnees recues du formulaire dans un fichier.
-    $hidden = str_replace(' ', '+', $hidden);
-    $file_content = base64_decode($hidden);
-    file_put_contents($location, $file_content);
-
-
-    // On check le nom et la description de la photo.
-    if (empty($name))
-    {
-        $name = "photo de " . $_SESSION['user_name'];
+    elseif (!isset($filtre)) {
+        banner_alert("vous devez selectionner un filtre pour enregistrer votre photo.");
     }
-    if (empty($description))
-    {
-        $description = "photo de " . $_SESSION['user_name'] . " avec le filtre " . $_POST['filtre'];
+    elseif (strlen($name) > 50 || strlen($dscription)) {
+      banner_alert("Commentaire trop long.");
     }
-
-    //On insere la photo dans la BDD.
-    $requete_insertion_image = "INSERT INTO image (location, owner, creation_time, name, description)
-               VALUES (:location, :id_user, :creation_time, :name, :description)";
-
-    $query_add_photo = $bdd->prepare($requete_insertion_image);
-    $result_add_photo = $query_add_photo->execute(array('location' => $location,
-                                                           'id_user' => $id_user,
-                                                           'creation_time' => date("YmdHis"),
-                                                           'name' => $name,
-                                                           'description' => $description));
-
-
-    $query_add_photo->closeCursor();
-
-    // Resultat de l'operation d'insertion.
-    if ($result_add_photo)
-    {
-        echo "<script> alert('la photo a bien ete sauvee.');</script>";
+    elseif (preg_match($pattern, $name) || preg_match($pattern, $description)) {
+        banner_alert("caracteres interdits ! seul les lettres et chiffres sont acceptes");
     }
-    else {
-        echo  "<script> alert('Une erreur est survenue, reesayez plus tard.');</script>";
-    }
-    if (is_file("upload/image"))
-    {
-        unlink("upload/image");
-    }
+    else { // TOUT EST OK ON SAVE L'IMAGE.
 
+        // creation d'un repertoire au nom de l'utilisateur si il n'en a pas.
+        if (!is_dir("img/" . $_SESSION['user_name']))
+        {
+            mkdir("img/" . $_SESSION['user_name']);
+        }
+
+        // ON cree une path pour le fichier a enregistrer.
+        $location = "img/" . $_SESSION['user_name'] . "/photo" . date("YmdHis") . ".png";
+        // On trouve l'id de l'utilisateur.
+        $query_user_id = $bdd->prepare("SELECT id FROM user WHERE pseudo = ?");
+        $query_user_id->execute(array($_SESSION['user_name']));
+        $result_user_id = $query_user_id->fetch();
+        $id_user = $result_user_id['id'];
+
+        //on transfere les donnees recues du formulaire dans un fichier.
+        $hidden = str_replace(' ', '+', $hidden);
+        $file_content = base64_decode($hidden);
+        file_put_contents($location, $file_content);
+
+
+        // On check le nom et la description de la photo.
+        if (empty($name))
+        {
+            $name = "photo de " . $_SESSION['user_name'];
+        }
+        if (empty($description))
+        {
+            $description = "photo de " . $_SESSION['user_name'] . " avec le filtre " . $_POST['filtre'];
+        }
+
+        //On insere la photo dans la BDD.
+        $requete_insertion_image = "INSERT INTO image (location, owner, creation_time, name, description)
+                   VALUES (:location, :id_user, :creation_time, :name, :description)";
+
+        $query_add_photo = $bdd->prepare($requete_insertion_image);
+        $result_add_photo = $query_add_photo->execute(array('location' => $location,
+                                                               'id_user' => $id_user,
+                                                               'creation_time' => date("YmdHis"),
+                                                               'name' => $name,
+                                                               'description' => $description));
+
+
+        $query_add_photo->closeCursor();
+
+        // Resultat de l'operation d'insertion.
+        if ($result_add_photo)
+        {
+            banner_alert('la photo a bien ete sauvee.');
+        }
+        else {
+            banner_alert('Une erreur est survenue, reesayez plus tard.');
+        }
+        if (is_file("upload/image"))
+        {
+            unlink("upload/image");
+        }
+    }
 }
-
-else if (isset($_POST) && $hidden && empty($filtre)) {
-    banner_alert("vous devez selectionner un filtre pour enregistrer votre photo.");
-}
-
-elseif (isset($_POST) && $hidden && empty($name)) {
-    banner_alert("Vous devez renseigner un nom a votre photo pour l'enregistrer.");
-}
-
 
  ?>
